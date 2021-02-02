e5d93cd30473ce9505e23c28dcbdd6d78a0dfee3
shadowaion <chiluka_sasha@mail.ru>
2021-02-03 02:13:43 +0300

    Added stylish function. Not tested yet.

--- src/genDiff.php
+++ src/genDiff.php
@@ -7,18 +7,37 @@ function typeValueToString($value)
      return trim(var_export($value, true), "'");
 }
 
-function typeNestedArrayAsString($arrayToType)
+function stringify($value, $replacer = ' ', $spacesCount = 1)
 {
-    $resultString = '';
+    $spacesNested = 0;
 
-    $resultString .= "{\n";
-    foreach ($arrayMergedKeysArr as $itemKey => $itemOne) {
+    $stringifyReduce = function ($value, $replacer, $spacesCount, $spacesNested) use (&$stringifyReduce) {
+        $resultString = '';
+        if (!is_array($value)) {
+            return toString($value);
+        } else {
+            $resultString .= "{\n";
+            $spacesNested += $spacesCount;
+            foreach ($value as $valueKey => $valueData) {
+                $replacerTimesSpaces = str_repeat($replacer, $spacesNested);
+                if (is_array($valueData)) {
+                    $currentArrString = $stringifyReduce($valueData, $replacer, $spacesCount, $spacesNested);
+                    $resultString .= $replacerTimesSpaces . $valueKey . ": " . $currentArrString . "\n";
+                } else {
+                    $currentString = $replacerTimesSpaces . toString($valueKey) . ": " . toString($valueData);
+                    $resultString .= $currentString . "\n";
+                }
+            }
+            $spacesNested -= $spacesCount;
+            $resultString .= str_repeat($replacer, $spacesNested) . "}";
+            return $resultString;
+        }
+    };
 
-    }
-    $resultString .= "}";
+    return $stringifyReduce($value, $replacer, $spacesCount, $spacesNested);
 }
 
-function genDiff($arrToOutAsString)
+function genDiff($parsedArrayOfFileOne, $parsedArrayOfFileTwo)
 {
     $resultArr = [];
 
@@ -45,7 +64,7 @@ function genDiff($arrToOutAsString)
         }
         if (isset($parsedArrayOfFileOne[$itemKey]) && isset($parsedArrayOfFileTwo[$itemKey])) {
             if (is_array($parsedArrayOfFileOne[$itemKey]) && is_array($parsedArrayOfFileTwo[$itemKey])) {
-                $childArr = findDiff($parsedArrayOfFileOne[$itemKey], $parsedArrayOfFileTwo[$itemKey]);
+                $childArr = genDiff($parsedArrayOfFileOne[$itemKey], $parsedArrayOfFileTwo[$itemKey]);
                 $resultArr[] = [
                     "key" => $itemKey, 
                     "firstArrValue" => $parsedArrayOfFileOne[$itemKey], 
@@ -83,4 +102,85 @@ function genDiff($arrToOutAsString)
         }
     }
     return $resultArr;
+}
+
+function typeNestedArrayAsString($arrayToType, $nestedLevel)
+{
+    $resultString = '';
+    $nextNestedLvl = $nestedLevel + 1;
+    $currSpaces = str_repeat ("    ", $nestedLevel);
+    $nextSpaces = str_repeat ("    ", $nextNestedLvl);
+
+    $resultString .= "{$currSpaces}{\n";
+    foreach ($arrayMergedKeysArr as $itemKey => $itemValue) {
+        if (is_array($itemValue)){
+            $childString = typeNestedArrayAsString($itemValue, $nextNestedLvl);
+            $resultString .= "{$nextSpaces}{$childString}\n";
+        } else {
+            $resultString .= "{$nextSpaces}{$itemKey}: {$itemValue}\n";
+        }
+    }
+    $resultString .= "{$currSpaces}}";
+
+    return $resultString;
+}
+
+function stylish($arrayToOutAsString, $nestedLevel)
+{
+    $resultString = '';
+    $nextNestedLvl = $nestedLevel + 1;
+    $spaces = str_repeat ("    ", $nestedLevel);
+
+    $resultString .= "{$spaces}{\n";
+    foreach ($arrToOutAsString as $key => $arr){
+        $keyOfStructure = $arr['key'];
+        $firstValueOfStructure = $arr['firstArrValue'];
+        $secondValueOfStructure = $arr['secondArrValue'];
+        switch ($arr['cmpResult']){
+            case 1:
+                if($arr['children'] === null && !is_array($firstValueOfStructure)){
+                    $resultString .= "{$spaces}  - {$keyOfStructure}: {$firstValueOfStructure}\n";
+                } elseif ($arr['children'] === null && is_array($firstValueOfStructure)){
+                    $childString = typeNestedArrayAsString($firstValueOfStructure, $nextNestedLvl);
+                    $resultString .= "{$keyOfStructure}: {$childString}\n";
+                } elseif ($arr['children'] !== null){
+                    $resultString .= stylish($arr['children'], $nextNestedLvl);
+                }
+                break;
+            case 2:
+                if($arr['children'] === null && !is_array($secondValueOfStructure)){
+                    $resultString .= "{$spaces}  + {$keyOfStructure}: {$secondValueOfStructure}\n";
+                } elseif ($arr['children'] === null && is_array($secondValueOfStructure)){
+                    $childString = typeNestedArrayAsString($secondValueOfStructure, $nextNestedLvl);
+                    $resultString .= "{$keyOfStructure}: {$childString}\n";
+                } elseif ($arr['children'] !== null){
+                    $resultString .= stylish($arr['children'], $nextNestedLvl);
+                }
+                break;
+            case 3:
+                if($arr['children'] === null){
+                    $resultString .= "{$spaces}    {$keyOfStructure}: {$firstValueOfStructure}\n";
+                } else {
+                    $resultString .= stylish($arr['children'], $nextNestedLvl);
+                }
+                break;
+            case 4:
+                    if($arr['children'] === null){
+                        if (!is_array($firstValueOfStructure)){
+                            $resultString .= "{$spaces}  - {$keyOfStructure}: {$firstValueOfStructure}\n";
+                        } else {
+                            $childString = typeNestedArrayAsString($firstValueOfStructure, $nextNestedLvl);
+                            $resultString .= "{$keyOfStructure}: {$childString}\n";
+                        }
+                        if (!is_array($secondValueOfStructure)){
+                            $resultString .= "{$spaces}  - {$keyOfStructure}: {$secondValueOfStructure}\n";
+                        } else {
+                            $childString = typeNestedArrayAsString($secondValueOfStructure, $nextNestedLvl);
+                            $resultString .= "{$keyOfStructure}: {$childString}\n";
+                        }
+                    }
+                break;
+        }
+    }
+    $resultString .= "{$spaces}}";
 }
