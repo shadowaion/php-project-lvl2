9fdb438611f49e9e2f23aaf6298bf303d9caa92e
shadowaion <chiluka_sasha@mail.ru>
2021-02-07 03:46:10 +0300

    Fixing errors

--- Formatters/plain.php
+++ Formatters/plain.php
@@ -4,24 +4,49 @@ namespace php\project\lvl2\Formatters\Plain;
 
 use function php\project\lvl2\src\Functions\typeValueToString;
 
+function getValue1($firstValueOfStructure, $firstValueType, $currParRoute): string
+{
+    if (!is_array($firstValueOfStructure)) {
+        $stringifyValue = typeValueToString($firstValueOfStructure);
+        if ($firstValueType === 'string') {
+            return "Property '{$currParRoute}' was updated. From '{$stringifyValue}' ";
+        } else {
+            return "Property '{$currParRoute}' was updated. From {$stringifyValue} ";
+        }
+    } else {
+        return "Property '{$currParRoute}' was updated. From [complex value] ";
+    }
+}
+
+function getValue2($secondValueOfStructure, $secondValueType): string
+{
+    if (!is_array($secondValueOfStructure)) {
+        $stringifyValue = typeValueToString($secondValueOfStructure);
+        if ($secondValueType === 'string') {
+            return "to '{$stringifyValue}'\n";
+        } else {
+            return "to {$stringifyValue}\n";
+        }
+    } else {
+        return "to [complex value]\n";
+    }
+}
+
 function plain($arrayToOutAsString, $parentsRoute = ''): string
 {
-    $resultString = '';
-
-    foreach ($arrayToOutAsString as $key => $arr) {
+    $resultArray = array_map(function ($arr) use ($parentsRoute){
         $keyOfStructure = $arr['key'];
         $firstValueOfStructure = $arr['firstArrValue'];
         $secondValueOfStructure = $arr['secondArrValue'];
-        $currentParentsRoute;
 
-        $currentParentsRoute = empty($parentsRoute) ? $keyOfStructure : "{$parentsRoute}.{$keyOfStructure}";
+        $currentParentsRoute = $parentsRoute === '' ? $keyOfStructure : "{$parentsRoute}.{$keyOfStructure}";
 
         switch ($arr['cmpResult']) {
             case 1:
                 if ($arr['children'] === null && !is_array($firstValueOfStructure)) {
-                    $resultString .= "Property '{$currentParentsRoute}' was removed\n";
+                    return "Property '{$currentParentsRoute}' was removed\n";
                 } elseif ($arr['children'] === null && is_array($firstValueOfStructure)) {
-                    $resultString .= "Property '{$currentParentsRoute}' was removed\n";
+                    return "Property '{$currentParentsRoute}' was removed\n";
                 }
                 break;
             case 2:
@@ -29,46 +54,31 @@ function plain($arrayToOutAsString, $parentsRoute = ''): string
                     $stringifyValue = typeValueToString($secondValueOfStructure);
                     if ($arr['secondValueType'] === 'string') {
                         $line = "Property '{$currentParentsRoute}' was added with value: '{$stringifyValue}'\n";
-                        $resultString .= $line;
+                        return $line;
                     } else {
-                        $resultString .= "Property '{$currentParentsRoute}' was added with value: {$stringifyValue}\n";
+                        return "Property '{$currentParentsRoute}' was added with value: {$stringifyValue}\n";
                     }
                 } elseif ($arr['children'] === null && is_array($secondValueOfStructure)) {
-                    $resultString .= "Property '{$currentParentsRoute}' was added with value: [complex value]\n";
+                    return "Property '{$currentParentsRoute}' was added with value: [complex value]\n";
                 }
                 break;
             case 3:
                 if ($arr['children'] !== null) {
                     $childString = plain($arr['children'], $currentParentsRoute);
-                    $resultString .= "{$childString}\n";
+                    return "{$childString}\n";
                 }
                 break;
             case 4:
                 if ($arr['children'] === null) {
-                    if (!is_array($firstValueOfStructure)) {
-                        $stringifyValue = typeValueToString($firstValueOfStructure);
-                        if ($arr['firstValueType'] === 'string') {
-                            $resultString .= "Property '{$currentParentsRoute}' was updated. From '{$stringifyValue}' ";
-                        } else {
-                            $resultString .= "Property '{$currentParentsRoute}' was updated. From {$stringifyValue} ";
-                        }
-                    } else {
-                        $resultString .= "Property '{$currentParentsRoute}' was updated. From [complex value] ";
-                    }
-                    if (!is_array($secondValueOfStructure)) {
-                        $stringifyValue = typeValueToString($secondValueOfStructure);
-                        if ($arr['secondValueType'] === 'string') {
-                            $resultString .= "to '{$stringifyValue}'\n";
-                        } else {
-                            $resultString .= "to {$stringifyValue}\n";
-                        }
-                    } else {
-                        $resultString .= "to [complex value]\n";
-                    }
+                    $partOne = getValue1($firstValueOfStructure, $arr['firstValueType'], $currentParentsRoute);
+                    $secondPart = getValue2($secondValueOfStructure, $arr['secondValueType']);
+                    return "{$partOne}{$secondPart}";
                 }
                 break;
         }
-    }
+    }, $arrayToOutAsString);
+
+    $resultString = implode('', $resultArray);
 
     $trimmedResult = rtrim($resultString);
 
