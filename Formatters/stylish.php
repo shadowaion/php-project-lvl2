286d4e1b83e5f5815529efb62551b580d02435e6
shadowaion <chiluka_sasha@mail.ru>
2021-02-08 02:30:29 +0300

    Fixing errors.

--- Formatters/stylish.php
+++ Formatters/stylish.php
@@ -43,62 +43,8 @@ function stylish($arrayToOutAsString, $nestedLevel = 0): string
     $nextNestedLvl = $nestedLevel + 1;
     $spaces = str_repeat("    ", $nestedLevel);
 
-    // $resultString .= "{\n";
-    // foreach ($arrayToOutAsString as $key => $arr) {
-    //     $keyOfStructure = $arr['key'];
-    //     $firstValueOfStructure = $arr['firstArrValue'];
-    //     $secondValueOfStructure = $arr['secondArrValue'];
-    //     switch ($arr['cmpResult']) {
-    //         case 1:
-    //             if ($arr['children'] === null && !is_array($firstValueOfStructure)) {
-    //                 $stringifyValue = typeValueToString($firstValueOfStructure);
-    //                 $resultString .= "{$spaces}  - {$keyOfStructure}: {$stringifyValue}\n";
-    //             } elseif ($arr['children'] === null && is_array($firstValueOfStructure)) {
-    //                 $childString = typeStylishNestedString($firstValueOfStructure, $nextNestedLvl);
-    //                 $resultString .= "{$spaces}  - {$keyOfStructure}: {$childString}\n";
-    //             }
-    //             break;
-    //         case 2:
-    //             if ($arr['children'] === null && !is_array($secondValueOfStructure)) {
-    //                 $stringifyValue = typeValueToString($secondValueOfStructure);
-    //                 $resultString .= "{$spaces}  + {$keyOfStructure}: {$stringifyValue}\n";
-    //             } elseif ($arr['children'] === null && is_array($secondValueOfStructure)) {
-    //                 $childString = typeStylishNestedString($secondValueOfStructure, $nextNestedLvl);
-    //                 $resultString .= "{$spaces}  + {$keyOfStructure}: {$childString}\n";
-    //             }
-    //             break;
-    //         case 3:
-    //             if ($arr['children'] === null) {
-    //                 $stringifyValue = typeValueToString($firstValueOfStructure);
-    //                 $resultString .= "{$spaces}    {$keyOfStructure}: {$stringifyValue}\n";
-    //             } else {
-    //                 $childString = stylish($arr['children'], $nextNestedLvl);
-    //                 $resultString .= "{$spaces}    {$keyOfStructure}: {$childString}\n";
-    //             }
-    //             break;
-    //         case 4:
-    //             if ($arr['children'] === null) {
-    //                 if (!is_array($firstValueOfStructure)) {
-    //                     $stringifyValue = typeValueToString($firstValueOfStructure);
-    //                     $resultString .= "{$spaces}  - {$keyOfStructure}: {$stringifyValue}\n";
-    //                 } else {
-    //                     $childString = typeStylishNestedString($firstValueOfStructure, $nextNestedLvl);
-    //                     $resultString .= "{$spaces}  - {$keyOfStructure}: {$childString}\n";
-    //                 }
-    //                 if (!is_array($secondValueOfStructure)) {
-    //                     $stringifyValue = typeValueToString($secondValueOfStructure);
-    //                     $resultString .= "{$spaces}  + {$keyOfStructure}: {$stringifyValue}\n";
-    //                 } else {
-    //                     $childString = typeStylishNestedString($secondValueOfStructure, $nextNestedLvl);
-    //                     $resultString .= "{$spaces}  + {$keyOfStructure}: {$childString}\n";
-    //                 }
-    //             }
-    //             break;
-    //     }
-    // }
-    // $resultString .= "{$spaces}}";
     $startBracket = "{\n";
-    $resultArray = array_map(function ($arr) use ($spaces, $nextNestedLvl) {
+    $resultArray = array_map(function ($arr) use ($spaces, $nextNestedLvl): string {
         $keyOfStructure = $arr['key'];
         $firstValueOfStructure = $arr['firstArrValue'];
         $secondValueOfStructure = $arr['secondArrValue'];
@@ -150,6 +96,7 @@ function stylish($arrayToOutAsString, $nestedLevel = 0): string
                 }
                 break;
         }
+        return "";
     }, $arrayToOutAsString);
     $resultString = implode('', $resultArray);
     $endBracket = "{$spaces}}";
