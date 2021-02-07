b8a7a9c72c9faf3c20438f69a7946f258e13b3ef
shadowaion <chiluka_sasha@mail.ru>
2021-02-07 04:00:07 +0300

    Fixing errors

--- Formatters/json.php
+++ Formatters/json.php
@@ -7,7 +7,7 @@ use function php\project\lvl2\src\Functions\typeValueToString;
 function jsonFormat($arrayToOutAsString): string
 {
     $jsonEncodeResult = json_encode($arrayToOutAsString, JSON_PRETTY_PRINT);
-    if (gettype($jsonEncodeResult) === 'string'){
+    if (gettype($jsonEncodeResult) === 'string') {
         return $jsonEncodeResult;
     } else {
         return typeValueToString($jsonEncodeResult);
